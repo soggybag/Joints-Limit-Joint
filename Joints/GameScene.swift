ef8682a1695deb3ec1c86186392f47dca63b87fc
Mitchell Hudson <mitchellhudson@mitchells-MacBook-Pro.local>
2016-07-13 16:12:05 -0700

    init
    Illustrates limit joint

--- Joints/GameScene.swift
+++ Joints/GameScene.swift
@@ -8,15 +8,48 @@
 
 import SpriteKit
 
+
 class GameScene: SKScene {
+    
+    var boxA: SKSpriteNode!
+    var boxB: SKSpriteNode!
+    var rope: SKPhysicsJointLimit!
+    var line: SKShapeNode!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: view.frame)
+        
+        let boxSize = CGSize(width: 60, height: 60)
+        boxA = SKSpriteNode(color: UIColor.orangeColor(), size: boxSize)
+        boxB = SKSpriteNode(color: UIColor.greenColor(), size: boxSize)
+        
+        addChild(boxA)
+        addChild(boxB)
+        
+        boxA.position.x = 100
+        boxB.position.x = 300
+        
+        boxA.position.y = 30
+        boxB.position.y = 30
+        
+        boxA.physicsBody = SKPhysicsBody(rectangleOfSize: boxSize)
+        boxB.physicsBody = SKPhysicsBody(rectangleOfSize: boxSize)
+        
+        boxA.name = "box"
+        boxB.name = "box"
+    
+        // Mark: Create Limit
+        rope = SKPhysicsJointLimit.jointWithBodyA(boxA.physicsBody!,
+            bodyB: boxB.physicsBody!,
+            anchorA: boxA.position,
+            anchorB: boxB.position)
+        
+        physicsWorld.addJoint(rope)
+        
+        line = SKShapeNode()
+        addChild(line)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -24,22 +57,30 @@ class GameScene: SKScene {
         
         for touch in touches {
             let location = touch.locationInNode(self)
+            let node = nodeAtPoint(location)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            if node.name == "box" {
+                print("Touch!")
+                let vector = CGVector(dx: 0, dy: 8000)
+                node.physicsBody?.applyForce(vector, atPoint: location)
+            }
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        let path = UIBezierPath()
+        path.moveToPoint(boxA.position)
+        path.addLineToPoint(boxB.position)
+        line.path = path.CGPath
+        line.lineWidth = 2
+        line.strokeColor = UIColor.whiteColor()
     }
 }
+
+
+
+
+
+
