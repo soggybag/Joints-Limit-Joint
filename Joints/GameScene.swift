eb18326b7211e9f8790b53f23d52f18209ab0869
Mitchell Hudson <soggybag@gmail.com>
2016-07-13 19:41:43 -0700

    Update GameScene.swift

--- Joints/GameScene.swift
+++ Joints/GameScene.swift
@@ -10,7 +10,7 @@
 
 /*
  
-This example sets up a set up chan links using pin joints.
+This example sets up a set up chain links using pin joints.
  
 Tapping a box will throw it in the air. Note tapping a chain link will 
  do nothing. The two boxes will stay connected via a series of smaller 
